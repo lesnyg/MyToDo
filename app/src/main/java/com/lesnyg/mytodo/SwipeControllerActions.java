5f308a6f2b4b9b749e5ca1a049eae5427cfcd36a
User <lesnyg@gmail.com>
2019-05-14 09:12:28 +0900

    swipe시 background에 색과 아이콘 넣기 , swipeController 분리

---
+++ app/src/main/java/com/lesnyg/mytodo/SwipeControllerActions.java
@@ -0,0 +1,9 @@
+package com.lesnyg.mytodo;
+
+public abstract class SwipeControllerActions {
+
+    public void onLeftClicked(int position) {}
+
+    public void onRightClicked(int position) {}
+
+}
